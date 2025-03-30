e80400976e1a82bd04176072039417aad18ca14b
Abhishek kumar <108456881+03abhishekit@users.noreply.github.com>
2025-03-30 12:29:03 +0530

    Update courseProgressApi.js

--- Frontend/src/features/api/courseProgressApi.js
+++ Frontend/src/features/api/courseProgressApi.js
@@ -1,59 +1,5 @@
 
 
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-// const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
-
-// export const courseProgressApi = createApi({
-//   reducerPath: "courseProgressApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: COURSE_PROGRESS_API,
-//     credentials: "include",
-//   }),
-//   endpoints: (builder) => ({
-//     getCourseProgress: builder.query({
-//       query: (courseId) => ({
-//         url: `/${courseId}`,
-//         method: "GET",
-//       }),
-//     }),
-//     updateLectureProgress: builder.mutation({
-//       query: ({ courseId, lectureId }) => ({
-//         url: `/${courseId}/lecture/${lectureId}/view`,
-//         method:"POST"
-//       }),
-//     }),
-
-//     completeCourse: builder.mutation({
-//         query:(courseId) => ({
-//             url:`/${courseId}/complete`,
-//             method:"POST"
-//         })
-//     }),
-//     inCompleteCourse: builder.mutation({
-//         query:(courseId) => ({
-//             url:`/${courseId}/incomplete`,
-//             method:"POST"
-//         })
-//     }),
-    
-//   }),
-// });
-// export const {
-// useGetCourseProgressQuery,
-// useUpdateLectureProgressMutation,
-// useCompleteCourseMutation,
-// useInCompleteCourseMutation
-// } = courseProgressApi;
-
-
-
-
-
-
-
-
-
 
 
 
@@ -62,7 +8,8 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+// const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+const COURSE_PROGRESS_API = "https://proskill-hub-backend.onrender.com";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
