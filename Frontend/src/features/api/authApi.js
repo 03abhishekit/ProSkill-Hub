7b94d7a7edbaa79a950433e382067c4bb9151a9d
Abhishek kumar <108456881+03abhishekit@users.noreply.github.com>
2025-03-30 12:27:02 +0530

    Update authApi.js

--- Frontend/src/features/api/authApi.js
+++ Frontend/src/features/api/authApi.js
@@ -4,118 +4,6 @@
 
 
 
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { userLoggedIn, userLoggedOut } from "../authSlice";
-
-// const USER_API = "http://localhost:8080/api/v1/user/";
-
-
-
-// export const authApi = createApi({
-//   reducerPath: "authApi",
-//   baseQuery: fetchBaseQuery(
-//     { baseUrl: USER_API,
-//          credentials: "include",
-//          prepareHeaders: (headers) => {
-//           const token = localStorage.getItem("token"); 
-//           if (token) {
-//             headers.set("Authorization", `Bearer ${token}`);
-//           }
-//           return headers;
-//         },
-//      }
-//     ),
-//   endpoints: (builder) => ({
-//     registerUser: builder.mutation({
-//       query: (inputData) => ({
-//         url: "register",
-//         method: "POST",
-//         body: inputData,
-//       }),
-//     }),
-//     loginUser: builder.mutation({
-//       query: (inputData) => ({
-//         url: "login",
-//         method: "POST",
-//         body: inputData,
-//       }),
-
-//       async onQueryStarted(_, { queryFulfilled, dispatch }) {
-//         try {
-//           const result = await queryFulfilled;
-//           if(result.data.token){
-//             localStorage.setItem("token", result.data.token);
-//           }
-//           dispatch(userLoggedIn({ user: result.data.user }));
-//         } catch (error) {
-//           console.error("Login failed:", error?.data?.message || "Unknown error");
-//         }
-//       },
-//     }),
-
-//     logoutUser : builder.mutation({
-//          query :()=>({
-//           url:"logout",
-//           method:"GET",
-//          }),
-
-//          async onQueryStarted(_, { queryFulfilled, dispatch }) {
-//           try {
-//             const result = await queryFulfilled;
-//             localStorage.removeItem("token"); 
-//             dispatch(userLoggedOut());
-//           } catch (error) {
-//             console.error("Logout failed:", error?.data?.message || "Unknown error");
-//           }
-//         },
-//     }),
-
-    
-//     loadUser: builder.query({
-//       query: () => ({
-//         url: "profile",
-//         method: "GET",
-       
-//       }),
-
-//       async onQueryStarted(_, { queryFulfilled, dispatch }) {
-//         try {
-//           const result = await queryFulfilled;
-//           dispatch(userLoggedIn({ user: result.data.user }));
-//         } catch (error) {
-//           if (error.error?.status === 400) { 
-//             localStorage.removeItem("token"); 
-//             dispatch(userLoggedOut());
-//           }
-//           console.error("Login failed:", error?.data?.message || "Unknown error");
-//         }
-//       },
-//     }),
-
-//     updateUser : builder.mutation({
-//         query:(formData)=>({
-//            url :"profile/update",
-//            method:"PUT",
-//            body:formData,
-//            credentials:"include"
-//         })
-//     })
-    
-//   }),
-// });
-
-// export const { 
-//   useRegisterUserMutation, 
-//   useLoginUserMutation,
-//    useLoadUserQuery, 
-//    useUpdateUserMutation, 
-//    useLogoutUserMutation
-//    } = authApi;
-
-
-
-
-
 
 
 
@@ -130,7 +18,9 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const USER_API = "http://localhost:8080/api/v1/user/"
+// const USER_API = "http://localhost:8080/api/v1/user/"
+const USER_API = "https://proskill-hub-backend.onrender.com"
+
 
 export const authApi = createApi({
     reducerPath:"authApi",
